d0c4a2b8d878d3eae8f8292eca8c619c96e30b0f
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-11-12 22:01:43 -0500

    add ProductDetails Component with React loaderData

--- src/views/Products.tsx
+++ src/views/Products.tsx
@@ -1,17 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import { getProducts } from "../services/ProductService";
+import ProductDetails from "../components/ProductDetails";
+import { Product } from "../types";
 
 
 export async function loader() {
-    
+
   const products = await getProducts()
-  console.log(products)
-  return {}
+  return products
 }
 
 
-
 export default function Products() {
+
+  const products = useLoaderData() as Product[]
+
+
   return (
     <>
 
@@ -26,6 +30,31 @@ export default function Products() {
         </Link>
 
       </div>
+
+
+      <div className="p-2">
+        <table className="w-full mt-5 table-auto">
+          <thead className="bg-sky-600 text-white">
+            <tr>
+              <th className="p-2">Producto</th>
+              <th className="p-2">Precio</th>
+              <th className="p-2">Disponibilidad</th>
+              <th className="p-2">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map(product => (
+              <ProductDetails
+
+                key={product.id}
+                product={product}
+
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
+
 
     </>
   )
